233f4e85a77789087b522460920e650fdb50acf2
asowjdan <asowjdan@naver.com>
2025-05-28 12:04:14 +0900

    명언 만들기 8단계

---
+++ src/main/java/com/back/WiseSaying.java
@@ -0,0 +1,31 @@
+package com.back;
+
+public class WiseSaying {
+    private int id;
+    private String content;
+    private String author;
+    public WiseSaying(int id, String content, String author) {
+        setId(id);
+        setContent(content);
+        setAuthor(author);
+    }
+    //getter setter
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getContent() {
+        return content;
+    }
+    public void setContent(String content) {
+        this.content = content;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
