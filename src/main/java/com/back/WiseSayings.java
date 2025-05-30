c0510bc8b8b8ad373095f2589e58dc08c5c09a82
heigangbae <j56233145@gmail.com>
2025-05-30 15:07:18 +0900

    first

---
+++ src/main/java/com/back/WiseSayings.java
@@ -0,0 +1,33 @@
+package com.back;
+
+public class WiseSayings {
+    private int id;
+    private String author;
+    private String content;
+
+    public WiseSayings(int id, String author, String content) {
+        this.id = id;
+        this.author = author;
+        this.content = content;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
