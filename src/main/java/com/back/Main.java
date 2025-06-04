dbb6d2e3ed52772a09081601fb255d8622096f60
YouSeok518 <luke2062@naver.com>
2025-06-04 21:56:50 +0900

    2단계

--- src/main/java/com/back/Main.java
+++ src/main/java/com/back/Main.java
@@ -8,12 +8,22 @@ public class Main {
 
         Scanner scanner=new Scanner(System.in); //사용자입력을 받는 기능을 하는코드
 
+
         while(true){
             System.out.print("명령) ");
             String str=scanner.nextLine().trim();//nextLine이라는 함수
             if(str.equals("종료")){//사용자가 입력한 값이 종료와 동일하면
                 break;
             }
+            else if(str.equals("등록")){
+                System.out.print("명언 : ");
+                String wiseSayingContent = scanner.nextLine().trim();//변수의 의미
+                System.out.print("작가 : ");
+                String wiseSayingAuthor = scanner.nextLine().trim();
+
+            }
+
+
         }
         scanner.close();
     }
