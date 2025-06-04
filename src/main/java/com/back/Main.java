0e95e4c3f1d0ce923608a7c906b33f8624b616a4
YouSeok518 <luke2062@naver.com>
2025-06-04 21:53:13 +0900

    11단계

---
+++ src/main/java/com/back/Main.java
@@ -0,0 +1,21 @@
+package com.back;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("== 명언 앱 ==");
+
+        Scanner scanner=new Scanner(System.in); //사용자입력을 받는 기능을 하는코드
+
+        while(true){
+            System.out.print("명령) ");
+            String str=scanner.nextLine().trim();//nextLine이라는 함수
+            if(str.equals("종료")){//사용자가 입력한 값이 종료와 동일하면
+                break;
+            }
+        }
+        scanner.close();
+    }
+}
+
