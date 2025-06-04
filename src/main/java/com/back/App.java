838b18be45b980060aa1946a3d599bab67288737
YouSeok518 <luke2062@naver.com>
2025-06-04 23:23:33 +0900

    8단계

--- src/main/java/com/back/App.java
+++ src/main/java/com/back/App.java
@@ -23,10 +23,14 @@ public class App {
                 actionList();
             } else if (cmd.startsWith("삭제")) {
                 actionDelete(cmd);
+            }else if (cmd.startsWith("수정")){
+                actionModify(cmd);
             }
         }
         scanner.close();
     }
+
+
     //진입점 끝
 
     //사용자 등록 응대? 담당
@@ -70,9 +74,34 @@ public class App {
         System.out.println("%d번 명언이 삭제되었습니다.".formatted(id));
     }
 
+    private void actionModify(String cmd) {
+        String[] cmdBits = cmd.split("=",2);
 
+        if (cmdBits.length < 2 || cmdBits[1].isEmpty()){
+            System.out.println("id를 입력해주세요.");
+            return;
+        }
 
+        int id = Integer.parseInt(cmdBits[1]);
 
+        WiseSaying wiseSaying = findbyId(id);
+
+        if (wiseSaying == null) {
+            System.out.println("%d번 명언은 존재하지 않습니다.".formatted(id));
+            return;
+        }
+
+        System.out.printf("명언(기존) : %s\n", wiseSaying.content);
+        System.out.print("명언 : ");
+        String content = scanner.nextLine().trim();
+
+        System.out.printf("작가(기존) : %s\n", wiseSaying.author);
+        System.out.print("작가 : ");
+        String author = scanner.nextLine().trim();
+
+        modify(wiseSaying, content, author);
+
+    }
 
 
 
@@ -124,4 +153,29 @@ public class App {
 
         return deleteIndex;
     }
+
+    void modify(WiseSaying wiseSaying, String content, String author) {
+        wiseSaying.content = content;
+        wiseSaying.author = author;
+    }
+
+
+    WiseSaying findbyId(int id) {
+        int index = findIndexById(id);
+
+        if (index == -1) return null;
+
+        return wiseSayings[index];
+    }
+
+    int findIndexById(int id) {
+        for (int i = 0; i <= wiseSayingsLastIndex; i++){
+            if (wiseSayings[i].id == id) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
 }
